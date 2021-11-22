f024fa1b2ea307fc113dd0768d7e0422c9ff05a5
KhaledTamerAly <k.tamer155@gmail.com>
2021-11-22 14:57:05 +0100

    master branch fix

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState } from 'react';
 
 function App() {
-  const[isDeleted,setIsDeleted] = useState(false);
-
-  function deleteFlight()
-  {
-    setIsDeleted(true);
-  }
-
-  return (
-    <div>
-     <DeleteButton id = "6196d1e15fae191b11dc5c0a" remove={isDeleted} onDelete={deleteFlight}/>
-     </div>
-  );
+  
+  return;
 }
 
 export default App;
