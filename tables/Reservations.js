422c9771b55de664738a525488ed01b3efdceb4b
KhaledTamerAly <k.tamer155@gmail.com>
2021-12-09 19:45:22 +0100

    second sprint start

---
+++ tables/Reservations.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reservationScheme = new Schema({
+    fName:{
+        type: String,
+        required: true
+    },
+    lName:{
+        type: String,
+        required: true
+    },
+    passportNumber: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    flightNumber: {
+        type: String,
+        unique:true
+    },
+    chosenSeats : {
+        type : [String]
+    }
+});
+
+module.exports = Reservations = mongoose.model('reservations', reservationScheme);
