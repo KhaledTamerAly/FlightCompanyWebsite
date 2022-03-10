885551dc018091867e98f002533a74977b933119
KhaledTamerAly <k.tamer155@gmail.com>
2022-03-11 00:55:37 +0200

    V.1.0.4

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ app.get('/', (req,res)=>{
 
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('./client/build'));
+    app.use(express.static('client/build'));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'));
     });
   }
 
